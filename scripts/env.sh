7ab963e602195a5adbc4eea42c5788d60d043475
iw1 <info@37-north.net>
2024-03-13 09:36:50 -0700

    Update env.sh

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-export PIHUB_OSC_IP="192.168.0.31"
-export PIBOT_1_OSC_IP="192.168.0.121"
-export PIBOT_2_OSC_IP="192.168.0.120"
+export PIHUB_OSC_IP="192.168.0.103" # your IP HERE
+export PIBOT_1_OSC_IP="192.168.0.101" # your IP HERE
+export PIBOT_2_OSC_IP="192.168.0.102" # your IP HERE
 
 export PIHUB_OSC_PORT="1337"
 export PIBOT_OSC_PORT="1338"
@@ -15,7 +15,7 @@ export PIBOT_SCRIPT_DIR="$SCRIPT_DIR"
 export PIBOT_HOME="$SCRIPT_DIR/../src/pibot"
 export PIHUB_HOME="$SCRIPT_DIR/../src/pihub/sequencer"
 
-export ISADORA_OSC_IP="192.168.1.223"
+export ISADORA_OSC_IP="192.168.0.200" # your IP HERE
 export ISADORA_OSC_PORT="1234"
 
 echo "Env script sourced."
